1c17e8bb10404f258ba9aaaaa09021c2ed3c6a82
fairgres <fairgres>
2020-10-06 09:27:41 +0000

    Initial commit

---
+++ src/test/java/fadjj/FadjjTest.java
@@ -0,0 +1,21 @@
+package fadjj;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class FadjjTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
